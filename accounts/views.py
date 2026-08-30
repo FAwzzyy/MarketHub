f941985ad30157913bd636e05ed3b0001bf95ae9
Ahmed Fawzy <ahmedfawzyy18@gmail.com>
2026-08-30 11:10:07 +0900

    Configure PostgreSQL and accounts setup

--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to MarketHub")
+
+
+def register(request):
+    form = UserCreationForm()
+
+    return render(request, "registration/register.html", {
+        "form": form
+    })
